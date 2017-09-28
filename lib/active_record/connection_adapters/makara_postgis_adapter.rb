4a94e6e2baf05fe12f276ea56ea979ffb5e81d60
Christian Frugard <cfrugard@lightower.com>
2017-09-28 15:58:18 -0400

    OK

--- lib/active_record/connection_adapters/makara_postgis_adapter.rb
+++ lib/active_record/connection_adapters/makara_postgis_adapter.rb
@@ -23,7 +23,6 @@ else
 
 end
 
-<<<<<<< HEAD
 module ActiveRecord
   module ConnectionAdapters
     class MakaraPostgisAdapter < ActiveRecord::ConnectionAdapters::MakaraAbstractAdapter
@@ -32,14 +31,6 @@ module ActiveRecord
         def visitor_for(*args)
           ActiveRecord::ConnectionAdapters::MakaraPostgisAdapter.visitor_for(*args)
         end
-=======
-
-module ActiveRecord
-  module ConnectionAdapters
-    class MakaraPostgisAdapter < ActiveRecord::ConnectionAdapters::MakaraAbstractAdapter
-      def self.visitor_for(*args)
-        ActiveRecord::ConnectionAdapters::PostGISAdapter.visitor_for(*args)
->>>>>>> 9a441f3c38a15a47f6390dcff446fb1a21548399
       end
 
       protected
@@ -47,13 +38,7 @@ module ActiveRecord
       def active_record_connection_for(config)
         ::ActiveRecord::Base.postgis_connection(config)
       end
-<<<<<<< HEAD
 
     end
   end
 end
-=======
-    end
-  end
-end
->>>>>>> 9a441f3c38a15a47f6390dcff446fb1a21548399
